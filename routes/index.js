2994256220d3098836fa7527ad9756736c21b18c
Patrick Drejerczak <drejerczak@aol.com>
2021-08-23 12:18:45 +0200

    get/put working

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,9 @@ var router = express.Router();
 const ticketController = require("../controller/ticket.controller");
 
 /* GET all tickets. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
+router.get("/ticket", ticketController.getAllTickets);
 
 // Create a new ticket
-router.post("/", ticketController.createTicket);
+router.post("/ticket", ticketController.createTicket);
 
 module.exports = router;
