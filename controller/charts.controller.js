29ed82f27ebaddaa008f54715bacdabe0c918df5
kimbua <nguyen.k.lien98@gmail.com>
2021-08-26 22:39:05 +0700

    add donut chart route

--- controller/charts.controller.js
+++ controller/charts.controller.js
@@ -1,6 +1,5 @@
 const utilsHelper = require("../helpers/utils.helper");
 const Posts = require("../models/Posts");
-const Teams = require("../models/Teams");
 const Users = require("../models/Users");
 const chartsController = {};
 var mongoose = require("mongoose");
@@ -8,20 +7,26 @@ var mongoose = require("mongoose");
 chartsController.getDonutChart = async (req, res, next) => {
   try {
     var id = mongoose.Types.ObjectId("6102a08043bc5e0728a765ca");
-    let user = await Users.findOne({ _id: id }).populate("team");
-    let user1 = await Users.findOne({ _id: id });
-    console.log(user);
-    console.log(user1);
-
+    // let user = await Users.findOne({ _id: id }).populate("team");
+    let user = await Users.findOne({ _id: id });
     const posts = await Posts.find({ team: user.team });
 
-    // let data = await Posts.find().limit(20);
+    let completedPosts = 0;
+    let pendingPosts = 0;
+    posts.map((post) => {
+      if (post.isApproved) {
+        completedPosts += 1;
+      } else {
+        pendingPosts += 1;
+      }
+    });
+    let data = [completedPosts, pendingPosts];
 
     const response = utilsHelper.sendResponse(
       res,
       200,
       true,
-      { posts },
+      { data },
       null,
       "Get all postss successfully."
     );
