d67e2f6392d5fcec1b3047712c9153ff75c0c40f
Patrick Drejerczak <drejerczak@aol.com>
2021-08-26 16:20:54 +0200

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const ticketApi = require('./ticket.api');
-router.use('/ticket', ticketApi);
-const fakeDataApi = require('./fakeData.api')
-router.use('/fakedata',fakeDataApi)
+const ticketApi = require("./ticket.api");
+router.use("/ticket", ticketApi);
+
+const fakeDataApi = require("./fakeData.api");
+router.use("/fakedata", fakeDataApi);
+
+const postsApi = require("./postsData.api");
+router.use("/postsdata", postsDataApi);
+
 module.exports = router;
